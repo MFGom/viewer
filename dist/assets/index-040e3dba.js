93bc85d5ce287a549dcd656bc8388bfed48581df
MFGom <156314159+MFGom@users.noreply.github.com>
2024-01-16 14:25:49 +0100

    Update index-040e3dba.js

--- dist/assets/index-040e3dba.js
+++ dist/assets/index-040e3dba.js
@@ -1,12 +1,1 @@
-window.addEventListener("thatOpen", async e => {
-    const {name: t, payload: n} = e.detail;
 
-    if (t === "openModel" && n) {
-        // Check if 'n' is defined before attempting to destructure
-        const {name: s, buffer: i} = n || {};
-
-        // Continue with your code
-        const p = await SF.load(i, s);
-        M1.scene.get().add(p);
-    }
-});
